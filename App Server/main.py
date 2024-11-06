3c46e0e94d1ff56cd43fe92c6db21b12e5178d8f
Carlos Moroni <carlosmoroni123@gmail.com>
2024-11-06 14:50:20 -0300

    Reapply "ateração para o proximo build do sistema, inicio da inteface grafica do lado servidor, parcialmente pronto, inicio do validador dos checkouts"
    This reverts commit 321383cdd489b7e0bcbabdacb27d1bf7250c2f38.

--- App Server/main.py
+++ App Server/main.py
@@ -1,12 +1,25 @@
-from utils.config import *
-from controllers.init_server import *
+# main.py
+import tkinter as tk
+from interface.interfaceGrafica import AppInterface
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = AppInterface(root)
+    root.mainloop()
+
+# from utils.config import *
+# from controllers.init_server import *
+# from interface.interfaceGrafica import *
 
 
-if __name__ == '__main__':
-    config = ler_configuracoes()
-    servidor_ip = config['servidor_ip']
-    servidor_porta = config['servidor_porta']
-    pasta_destino = config['pasta_destino']
+# if __name__ == '__main__':
+#     config = ler_configuracoes()
+#     servidor_ip = config['servidor_ip']
+#     servidor_porta = config['servidor_porta']
+#     pasta_destino = config['pasta_destino']
     
-    print(ascii_art)
-    iniciar_servidor(servidor_ip, servidor_porta, pasta_destino)
+#     iniciar_servidor(servidor_ip, servidor_porta, pasta_destino)
+#     print(ascii_art)
+
+
+# # def button_init_server():   
